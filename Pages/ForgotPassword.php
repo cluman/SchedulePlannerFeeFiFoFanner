2e374a870cb4f8a441b3868ed3b647ac5d3a9633
Raphael Silva <raphaelrs55@hotmail.com>
2014-11-24 02:03:34 -0600

    New file

--- Pages/ForgotPassword.php
+++ Pages/ForgotPassword.php
@@ -1,1 +1,21 @@
-l
+<html>
+
+<body>
+
+<form action="ForgotPasswordProcessing.php" onSubmit="return validateForm();" method="post" name="Form">
+E-mail: <input id="email" type="text" name="email" value = ""><br>
+<input type="submit">
+</form>
+
+<br><br><br>
+<a href='Login.php'>Cancel</a>
+
+<script>
+function validateForm()
+{
+return true;
+}
+</script>
+
+</body>
+</html>
