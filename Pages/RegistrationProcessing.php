e808c75248240c3f5e5d7aa91ceb4fd4279ba428
Raphael Silva <raphaelrs55@hotmail.com>
2014-11-24 20:39:26 -0600

    Cleaned code

--- Pages/RegistrationProcessing.php
+++ Pages/RegistrationProcessing.php
@@ -3,13 +3,12 @@
 
 <?php
 
-session_start();
-
-header('Location: EmailConfirmationForm.php');
+require 'DatabaseConnection.php';
 
 $password = $_POST["password"];
 $email = $_POST["email"];
 
+session_start();
 $_SESSION['email'] = $email;
 
 $salt = rand(100000, 999999);
@@ -19,48 +18,26 @@ $confirmation_code = randStr(8);
 $encrypted_password = sha1($salt . $password);
 
 
-//---------------------
-
-$server = 'localhost';
-$base      = 'Scheduler';
-$user  = 'root';
-$passwd    = '';
-$link     = mysql_connect($server, $user, $passwd);
-$db          = mysql_select_db($base,$link);
-if(!$link)
-{
-    echo "Error connecting database!";exit();
-}
-
-// Create connection
-$conn = new mysqli($server, $user, $passwd, $base);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-
-$result = $conn->query("SELECT code FROM Registration WHERE email='$email'");
+$result = $conn->query("SELECT * FROM Registration WHERE email='$email'");
 
 if ($result->num_rows) {
-	echo ("E-mail already registered! <br>"); 
-	echo ("Click here to confirm your email. <br>"); //TODO
-	exit(); 
+	echo ("This e-mail is already registered, but hasn't been confirmed. <br>"); 
+	echo ("<a href='EmailConfirmation.php'>Click here</a> to confirm your email. <br>"); 
+	exit();
 }
 
 $result2 = $conn->query("SELECT * FROM User WHERE email='$email'");
 if ($result2->num_rows) {
 	echo ("E-mail already registered! <br>"); 
-	echo ("Click here to login. <br>"); //TODO
+	echo ("<a href='Login.php'>Click here</a> to login. <br>"); 
 	exit();
 }
 
 $sql = "INSERT INTO registration (email, password, salt, code) VALUES('$email', '$encrypted_password', '$salt', '$confirmation_code')";
 
-if (mysql_query($sql) == TRUE) {
-	echo "New record created successfully.<br>";
-} else { 
-	echo "Something went wrong inserting into the database.... Scream for help! <br>";
+if (mysql_query($sql) == FALSE) 
+{
+	die ("Something went wrong inserting into the database.... Scream for help!");
 }
 
 
@@ -83,14 +60,12 @@ require "email_config/SendEmailConfig.php";
 if (SendEmail ($to, $subject, $content, $fromName))
 {
 	echo "<script>alert('Confirmation code sent via e-mail.');</script>";
+	echo "<script>window.location = 'EmailConfirmation.php'</script>";
 }
 else
 {
-	echo "Couldn't send confirmation code via e-mail. Please, try to <a href='ResetCode.php'>send your code again</a> later.<br>";
-	exit();
-}
-	
-	
+	echo "Couldn't send confirmation code via e-mail. Please, try to <a href='ResendCode.php'>send your code again</a> later.<br>";
+}	
 	
 function randStr($length) {
     $chars = '123456789BCDFGHJKLMNPQRSTVWXYZ'; //No vowels so it won't write awkward words; No zero so it won't be mistaken with the letter O
