f15a5dfe18f42fcc04f674c3f5756158da9bb4a7
Raphael Silva <raphaelrs55@hotmail.com>
2014-11-24 20:39:14 -0600

    Deleted test code

--- Pages/Registration.php
+++ Pages/Registration.php
@@ -2,16 +2,11 @@
 <body>
 
 <form action="RegistrationProcessing.php" onSubmit="return validateForm();" method="post" name="Form">
-E-mail: <input id="email" type="text" name="email" value = r+"aaa@"><br>
-Password: <input type="password" name="password" value = "123"><br>
-Retype password: <input type="password" name="password_confirmation" value = "123"><br>
+E-mail: <input id="email" type="text" name="email"><br>
+Password: <input type="password" name="password"><br>
+Retype password: <input type="password" name="password_confirmation"><br>
 <input type="submit">
 </form>
-
-<script>
-var r = Math.floor((Math.random() * 999) + 1);;
-document.getElementById('email').value=r+"aaa@";
-</script>
 
 <script>
 
@@ -27,8 +22,8 @@ function validateForm() {
 	var pass1 = document.forms["Form"]["password"].value;
 	var pass2 = document.forms["Form"]["password_confirmation"].value;
 	
-	if (pass1 == "") {
-		alert ("Please choose a password");
+	if (pass1.length < 4) {
+		alert ("Please choose a password of at least 4 characters");
 		return false;
 	}
 	
