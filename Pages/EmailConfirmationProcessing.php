de1d91597497e5b0484c3e01b494f4cd3136a17e
Raphael Silva <raphaelrs55@hotmail.com>
2014-11-24 20:36:34 -0600

    Cleaned code and corrected some things

--- Pages/EmailConfirmationProcessing.php
+++ Pages/EmailConfirmationProcessing.php
@@ -1,25 +1,21 @@
 <?php
 
-header('Location: Login.php');
+require 'DatabaseConnection.php';
 
 $entered_confirmation_code = $_POST["confirmation_code"];
 $email = $_POST["email"];
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Scheduler";
-$db = mysql_connect($servername, $username, $password) or die("Database Error");
-mysql_select_db($dbname,$db);
+session_start();
+$_SESSION['email'] = $email;
 
 //Verify if e-mail is already registered
-/* $sql = "SELECT * FROM User WHERE email='$email'";
+$sql = "SELECT * FROM User WHERE email='$email'";
 $resource = mysql_query($sql);
 if($resource === false)
 {	die("Database Error <br>"); }
 $grab = mysql_fetch_assoc($resource);
 if (mysql_num_rows($resource) > 0) 
-{   echo "E-mail already confirmed! <br>Try <a href='Login.php'>login</a>."; die(); } */ //Commented only for tests
+{   echo "E-mail already confirmed! <br>Try <a href='Login.php'>login</a>."; die(); } 
 
 $sql = "SELECT * FROM Registration WHERE email='$email'";
 $resource = mysql_query($sql);
@@ -28,46 +24,40 @@ if($resource === false)
 
 $grab = mysql_fetch_assoc($resource);
 if (mysql_num_rows($resource) <= 0) 
-{   echo "E-mail not registered."; }
+{   echo "<script>alert('E-mail not registered.')</script>"; 
+	echo "<script>window.location = 'EmailConfirmation.php'</script>";}
 
 else
 {
 	$confirmation_code = $grab['code'];
-	//echo $code . "<br><br>";
 	
 	if ($entered_confirmation_code != $confirmation_code) 
 	{
-		die ("Invalid code!");
+		echo "<script>alert('Invalid code!')</script>";
+		echo "<script>window.location = 'EmailConfirmation.php'</script>";
 	}
 	else 
 	{
 		$password = $grab['password'];
 		$salt = $grab['salt'];
 					
-		if (mysql_query("INSERT INTO user (email, password, salt) VALUES('$email', '$password', '$salt')"))
-			echo "Registration succeeded. <br>";
-		//else
-			//die ("Couldn't register. Please, try again later or try again with a different e-mail. <br>"); //Commented only for tests
+		if (mysql_query("INSERT INTO user (email, password, salt) VALUES('$email', '$password', '$salt')") == false)			
+			die ("Couldn't register. Please, try again later or try again with a different e-mail. <br>"); 
 		
-		/* if(mysql_query("DELETE FROM registration WHERE email='$email'") == false)
+		if(mysql_query("DELETE FROM registration WHERE email='$email'") == false)
 		{
 			echo "Error in temporary database. Try <a href='Login.php'>login</a>.";
 			die();
 		}		
-		else
-			{
-				echo "Try <a href='Login.php'>login</a>.";
-			} */ //Commented only for tests
+		// else
+			// {
+				// echo "Try <a href='Login.php'>login</a>.";
+			// } 
 		
 		echo "<script> alert ('Confirmation succeeded.') </script>";
+		echo "<script>window.location = 'Login.php'</script>";
 	}
-	
-    /* echo "<table><tr><th>ID</th><th>Name</th></tr>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["code"]."</td><td>".$row["username"]." ".$row["email"]."</td></tr>";
-    }
-    echo "</table>"; */
+
 } 
 
 ?>
