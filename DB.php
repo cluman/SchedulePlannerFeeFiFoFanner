b0aed3ca0991b10a7955db7f45edcaedc69f9eea
cluman <cluman@uark.edu>
2014-12-05 01:14:26 -0600

    Update DB.php
    convertAM function needs work. After that is fixed the sort function should work.

--- DB.php
+++ DB.php
@@ -6,6 +6,7 @@
 <p id="idd"></p>
 <p id="time"></p>
 <p id="sort">push the button asshole</p>
+<h id="teem"></h>
 <?php
 require 'DBconnection.php';
 
@@ -34,7 +35,7 @@ echo "    Semester  : \"${f3}\",\n";
 echo "    Hours     : ${f4},\n";
 echo "    Time      : \"${f5}\",\n";
 echo "    TimeInt   : 0,\n";
-echo "	  GetTime   : function() { var temp; temp = this.Time.length - 19; return this.Time.slice(temp, temp+8);},\n";
+echo "    GetTime   : function() { var temp; temp = this.Time.length - 19; return this.Time.slice(temp, temp+8);},\n";
 echo "    Display   : function() { return this.ID+this.Title+this.Semester+this.Hours+this.Time;}\n};\n";
 echo "Courselist[${h}] = classs${h};\n";
 
@@ -52,12 +53,16 @@ function getTime (str)
     return str.slice(temp, temp+6);
 }
 
-function convert(string str){
+function convert(str){
     if(str.indexOf("PM") > -1)
-        convertPM(str);
+        str = convertPM(str);
+    else 
+        str = convertAM(str);
+
+    return str;
 }
 
-function convertPM(string str){
+function convertPM(str){
         if(str.indexOf("1") > -1 && str.indexOf("2") == -1)
             return 13;
         else if(str.indexOf("2") > -1)
@@ -82,10 +87,42 @@ function convertPM(string str){
             return 23;
         else
             return 12;
-}  
+} 
+
+function convertAM(str){
+        if(str.indexOf("1") > -1 && str.indexOf("2") == -1)
+            return 1;
+        else if(str.indexOf("2") > -1 && (str.indexOf("1") > 0 || str.indexOf("4") > 0 || str.indexOf("3") > 0 || str.indexOf("5") > 0 && str.indexOf("0") > 0))
+            return 2;
+        else if(str.indexOf("3") > -1 && (str.indexOf("1") > 0 || str.indexOf("4") > 0 || str.indexOf("2") > 0 || str.indexOf("5") > 0 && str.indexOf("0") > 0))
+            return 3;
+        else if(str.indexOf("4") > -1 && (str.indexOf("1") > 0 || str.indexOf("2") > 0 || str.indexOf("3") > 0 || str.indexOf("5") > 0 && str.indexOf("0") > 0))
+            return 4;
+        else if(str.indexOf("5") > -1 && (str.indexOf("1") > 0 || str.indexOf("4") > 0 || str.indexOf("3") > 0 || str.indexOf("2") > 0 && str.indexOf("0") > 0))
+            return 5;
+        else if(str.indexOf("6") > -1 && (str.indexOf("1") > 0 || str.indexOf("4") > 0 || str.indexOf("3") > 0 || str.indexOf("5") > 0 || str.indexOf("2") > 0 && str.indexOf("0") > 0))
+            return 6;
+        else if(str.indexOf("7") > -1 && (str.indexOf("1") > 0 || str.indexOf("4") > 0 || str.indexOf("3") > 0 || str.indexOf("5") > 0 || str.indexOf("2") > 0 && str.indexOf("0") > 0))
+            return 7;
+        else if(str.indexOf("8") > -1 && (str.indexOf("1") > 0 || str.indexOf("4") > 0 || str.indexOf("3") > 0 || str.indexOf("5") > 0 || str.indexOf("2") > 0 && str.indexOf("0") > 0))
+            return 8;
+        else if(str.indexOf("9") > -1 && (str.indexOf("1") > 0 || str.indexOf("4") > 0 || str.indexOf("3") > 0 || str.indexOf("5") > 0 || str.indexOf("2") > 0 && str.indexOf("0") > 0))
+            return 9;
+        else if(str.indexOf("10") > -1 && (str.indexOf("4") > 0 || str.indexOf("3") > 0 || str.indexOf("5") > 0 || str.indexOf("2") > 0 && str.indexOf("0") > 0))
+            return 10;
+        else if(str.indexOf("11") > -1 && (str.indexOf("4") > 0 || str.indexOf("3") > 0 || str.indexOf("5") > 0 || str.indexOf("2") > 0 && str.indexOf("0") > 0))
+            return 11;
+        else 
+            return 12;*/
+} 
 
 function sortList(){
-
+for(var p = 0; p < 6; p++){
+  var teim = Courselist[p].GetTime();
+  var teem = convert(teim);
+      document.getElementById("teem").innerHTML += teem + "<br>";
+  Courselist[p].TimeInt = teem;
+}
 Courselist.sort(function(a,b){
     return a.TimeInt - b.TimeInt;
 })
@@ -98,8 +135,8 @@ for(x = 0; x < 6; x++){
 }
     
 for(var h = 0; h < 6; h++){
-//document.getElementById("idd").innerHTML += Courselist[h].Display() + "<br>";
-document.getElementById("time").innerHTML += Courselist[h].GetTime() + "<br>";
+document.getElementById("idd").innerHTML += Courselist[h].Display() + "<br>";
+document.getElementById("time").innerHTML += Courselist[h].TimeInt + "<br>";
 }
 </script>
 
@@ -109,3 +146,4 @@ Click me to sort classes.</button>
 
 </body>
 </html>
+
